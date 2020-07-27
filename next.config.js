40897b6e36521fcec29239e5c103b2c81e7e4f79
galanggg <tugasgalang@gmail.com>
2020-07-27 17:59:31 +0800

    Add some configuration

--- next.config.js
+++ next.config.js
@@ -1,36 +1,42 @@
+const withPlugins = require('next-compose-plugins')
 const withImages = require('next-images')
-module.exports = withImages()
-
-// module.exports = {
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       issuer: {
-//         test: /\.(js|ts)x?$/,
-//       },
-//       use: ['@svgr/webpack'],
-//     })
-
-//     return config
-//   },
-// }
-
-// const withSass = require('@zeit/next-sass')
-// const withCSS = require('@zeit/next-css')
-
-// module.exports = withCSS(
-//   withSass({
-//     webpack(config, options) {
-//       config.module.rules.push({
-//         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-//         use: {
-//           loader: 'url-loader',
-//           options: {
-//             limit: 100000,
-//           },
-//         },
-//       })
-//       return config
-//     },
-//   }),
-// )
+const optimizedImages = require('next-optimized-images')
+//Tailwind
+const withSass = require('@zeit/next-sass')
+const tailwindCss = require('tailwindcss')
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      // these are the default values so you don't have to provide them if they are good enough for your use-case.
+      // but you can overwrite them here with any valid value you want.
+      inlineImageLimit: 8192,
+      imagesFolder: 'images',
+      imagesName: '[name]-[hash].[ext]',
+      handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'],
+      removeOriginalExtension: false,
+      optimizeImages: true,
+      optimizeImagesInDev: true,
+      mozjpeg: {
+        quality: 80,
+      },
+      optipng: {
+        optimizationLevel: 3,
+      },
+      pngquant: false,
+      gifsicle: {
+        interlaced: true,
+        optimizationLevel: 3,
+      },
+      svgo: {
+        // enable/disable svgo plugins here
+      },
+      webp: {
+        preset: 'default',
+        quality: 75,
+      },
+      imageTrace: {},
+    },
+  ],
+  withImages(),
+])
