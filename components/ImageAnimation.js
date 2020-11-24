b0b9d88177d85737baca74206315449d418a5bc4
adelanegara <adelangr822@gmail.com>
2020-11-24 14:50:02 +0800

    scroll trigger added

--- components/ImageAnimation.js
+++ components/ImageAnimation.js
@@ -1,29 +1,64 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { TimelineLite, Power2, gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+
 const ImageAnimation = (props) => {
   let image = useRef(null)
   let animateOverlay = useRef(null)
-  // let imgContainer = useRef(null)
 
   useEffect(() => {
-    let tl = new TimelineLite()
+    gsap.registerPlugin(ScrollTrigger)
+    let tl = gsap.timeline()
+
+    ScrollTrigger.create({
+      // trigger: '.image-animate__wrapper',
+      trigger: image,
+      start: 'top center',
+      end: 'bottom top',
+      scrub: true,
+      // toggleClass: '.visible', //this toggles the specified CSS class on the trigger element
+      onEnter: () => {
+        console.log('enter')
+        tl.play()
+      }, //this fires the drawPoints function when the trigger enters the scroller from above
+    })
 
     // const GSAP = require('gsap/CSSRulePlugin')
     // const { CSSRulePlugin } = GSAP
     // let animateOverlay = CSSRulePlugin.getRule('.image-animate__wrapper:after')
 
     tl.from(animateOverlay, 1, {
+      // ScrollTrigger: {
+      //   trigger: '.image-animate__wrapper',
+      //   start: 'top center',
+      //   end: 'bottom center',
+      //   scrub: true, //this tells GSAP to link animation progress to scroll progress
+      // },
       width: '0%',
       ease: Power2.easeInOut,
     })
       .to(image, 0.5, {
+        // ScrollTrigger: {
+        //   trigger: '.image-animate__wrapper',
+        //   start: 'top center',
+        //   end: 'bottom center',
+        //   scrub: true, //this tells GSAP to link animation progress to scroll progress
+        // },
         autoAlpha: 1,
       })
       .to(animateOverlay, 1, {
+        // ScrollTrigger: {
+        //   trigger: '.image-animate__wrapper',
+        //   start: 'top center',
+        //   end: 'bottom center',
+        //   scrub: true, //this tells GSAP to link animation progress to scroll progress
+        // },
         width: '0%',
         ease: Power2.easeInOut,
       })
+    tl.pause()
   }, [])
+
   return (
     <>
       <div className="image-animate__wrapper">
